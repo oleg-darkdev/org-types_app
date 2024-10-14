d3d448945b091153fb9796511d9ea2d224fe86da
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:14:48 +0300

    alfa v.

---
+++ src/lib/entities/ui/app/RolesList.svelte
@@ -0,0 +1,12 @@
+<script>
+  import {rolesList} from '$lib/shared';
+</script>
+
+{#each rolesList as role (role.id)}
+
+  <div class='text-2xl max-w-sm'>{role.name}</div>
+{/each}
+
+<style>
+
+</style>
