a51219185573a522498dc42fff6153079231b3dc
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:13:37 +0300

    upd api

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -1,4 +1,40 @@
-// import BoardgameSection from './ui/landing/BoardgameSection.svelte';
+import Hero from './landing/Hero.svelte';
+import Footer from './landing/Footer.svelte';
+import Header from './landing/Header.svelte';
+import Features from './landing/Features.svelte';
+import OtherBoardgames from './landing/OtherBoardgames.svelte';
+import CtaLight from './landing/CtaLight.svelte';
+import Hybridboardgame from './landing/Hybridboardgame.svelte';
+import OrgTypesList from './landing/OrgTypesList.svelte';
+import CtaColoured from './landing/CtaColoured.svelte';
+import Faq from './landing/Faq.svelte';
+import FullWidthBanner from './landing/FullWidthBanner.svelte';
+import Intro from './landing/Intro.svelte';
+import Divider from './landing/Divider.svelte';
+import OrgCategorySection from './landing/OrgCategorySection.svelte';
+// import Footer from './landing/Footer.svelte';
+// import Footer from './landing/Footer.svelte';
+// import Footer from './landing/Footer.svelte';
+// import Footer from './landing/Footer.svelte';
+// import Footer from './landing/Footer.svelte';
 
 
-// export {  };
+// app
+import HeaderApp from './app/HeaderApp.svelte';
+import VerifierRoleScreen from './app/VerifierRoleScreen.svelte';
+import TimerRoleScreen from './app/TimerRoleScreen.svelte';
+import GuideRoleScreen from './app/GuideRoleScreen.svelte';
+import FooterApp from './app/FooterApp.svelte';
+import AdvancedColorsRoleScreen from './app/AdvancedColorsRoleScreen.svelte';
+import BasicColorsRoleScreen from './app/BasicColorsRoleScreen.svelte';
+import EvolvingColorsRoleScreen from './app/EvolvingColorsRoleScreen.svelte';
+// import HeaderApp from './app/Header.svelte';
+// import HeaderApp from './app/Header.svelte';
+// import HeaderApp from './app/Header.svelte';
+// import HeaderApp from './app/Header.svelte';
+// import HeaderApp from './app/Header.svelte';
+// import HeaderApp from './app/Header.svelte';
+
+
+
+export { Hero, HeaderApp, AdvancedColorsRoleScreen, BasicColorsRoleScreen, EvolvingColorsRoleScreen, FooterApp, GuideRoleScreen, VerifierRoleScreen, TimerRoleScreen, Footer, Header, Features, OtherBoardgames, CtaLight, Divider, Hybridboardgame, OrgCategorySection, OrgTypesList, CtaColoured, Faq, FullWidthBanner, Intro,   };
