2df9528cee084aaf61e759022b434483e2cffe33
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:15:35 +0300

    for refactoring

---
+++ src/lib/widgets/landing/Header.svelte
@@ -0,0 +1,58 @@
+<script>
+
+</script>
+
+<!-- <div class="date">
+      <div class="padding">
+        <div class="datecentre">
+          <div class="centre w-clearfix">
+            <div id="date1" class="javadate">Sat Jun 8</div>
+            <div id="time1" class="javatime">19:19</div>
+            <div class="dateyear">2038</div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+ <!-- </a><a href="index.html" aria-current="page" class="logo w-inline-block w--current" alt="Homepage Botton"><img src="images/logo-02.svg" alt="Memory Work Logo" class="lovosvg"></a></div> -->
+<!-- sticky-nav -->
+<header id="nav" class="">
+  <!-- <div class="menu w-clearfix">
+    <div class="menu-section">
+      <div class="padding w-clearfix">
+        <a data-w-id="a4582e6d-21b9-f34d-cdd0-688657dbb716" href="#" class="hamburger w-inline-block" alt="Open Menu Button">
+          <div class="hidden-menu-close"></div>
+          <div  class="hidden-menu-open"></div>
+          <div class="hamdashholder">
+            <div class="hamdashes _1"></div>
+            <div class="hamdashes _2"></div>
+            <div class="hamdashes _3"></div>
+          </div>
+
+    </div>
+  </div>
+
+  <div data-w-id="0c4c6414-9a21-698b-2810-b679ec649f92" style="width:100%;height:0PX" class="menuopen">
+    <a href="/" alt="" class="menulink w-inline-block">
+      <div class="menurainbow"></div>
+      <div class="menuspace w-clearfix">
+        <div class="menucricle"><img src="images/volume.svg" loading="lazy" alt="" class="menuiconimg"></div>
+        <div class="menutext">Lorem impsum</div>
+      </div>
+    </a>
+
+  </div> -->
+
+  <div class="_12-col wrapper">
+    <a id="w-node-_0a70b4b6-ab25-19b0-227c-86c932d6b60c-32d6b60a" href="/" aria-current="page" class="logo-container w-inline-block w--current">
+      <img src="/images/logo.svg" loading="lazy" alt="Fujee Design Logo" class="logo pointer">
+    </a>
+    <div id="w-node-bc8188c3-82c0-f7de-5492-ab5d605d99e9-32d6b60a">
+      <a href="/app" class="nav-link">👉 Work</a>
+    </div>
+  </div>
+</header>
+
+
+<style>
+
+</style>
