46e787360946212bfe0661030a9719bdf7c894fa
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:11:36 +0300

    alfa v.

---
+++ src/lib/widgets/landing/Hybridboardgame.svelte
@@ -0,0 +1,123 @@
+<script>
+
+</script>
+
+<div class="section">
+  <div class="wrapper">
+    <div class="w-layout-grid _12-col">
+      <div class="big-title-wrap headline">
+        <h2 class="header-absolute">Hybrydowa edukacyjna gra planszowa</h2>
+        <div class="section-gradient">
+          <img src="/images/rounded_section_gradient.png" loading="lazy" alt="" class="saturate">
+        </div>
+      </div>
+    </div>
+    <div class="_12-col packages">
+      <div class="boardgame-title table-heading">
+        <h3 class="no-margin-bottom">Gra planszowa</h3>
+      </div>
+      <p  class="boardgame-item-1 table-text-block">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p  class="boardgame-item-2 table-text-block ">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p  class="boardgame-item-3 table-text-block">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p  class="boardgame-item-4 table-text-block">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p  class="boardgame-item-5 table-text-block last">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+
+      <div class="app-title table-heading">
+        <h3 class="no-margin-bottom">Aplikacja webowa</h3>
+      </div>
+
+      <p class="app-item-1 table-text-block">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p  class="app-item-2 table-text-block last">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+
+
+
+
+
+      <div id="w-node-ffe66e36-d7b1-f9c7-5988-a34ded3301ac-9531edc0" class="pros-cons teal">Funkcja </div>
+      <div id="w-node-_3609cd28-0121-e907-8fbc-9bb6ce000449-9531edc0" class="pros-cons teal">Funkcja </div>
+
+      <div id="w-node-_06ba7682-bdfc-043b-dcc1-4b0deae54df2-9531edc0" class="pros-cons teal visible-mobile">Funkcja</div>
+      <div id="w-node-_8845e17c-5372-e680-4d7b-ea636c2b0c4f-9531edc0" class="pros-cons teal visible-mobile">Funkcja</div>
+
+    </div>
+  </div>
+</div>
+
+
+<style>
+.app-title {
+grid-area: 1/9/2/13;
+}
+.app-item-1 {
+grid-area: 2/9/3/13;
+}
+.app-item-2 {
+grid-area: 3/9/4/13;
+}
+.big-title-wrap {
+grid-area: 1/2/2/10;
+}
+
+.boardgame-item-1 {
+grid-area: 2/5/3/9;
+}
+.boardgame-item-2 {
+grid-area: 3/5/4/9;
+}
+
+.boardgame-item-3 {
+grid-area: 4/5/5/9;
+}
+.boardgame-item-4 {
+grid-area: 5/5/6/9;
+}
+.boardgame-item-5 {
+grid-area: 6/5/7/9;
+}
+.boardgame-title {
+grid-area: 1/5/2/9;
+}
+
+
+
+@media screen and (max-width: 991px) {
+.boardgame-item-1, .boardgame-item-2, .boardgame-item-3, .boardgame-item-4, .boardgame-item-5 {
+grid-column: 2/3;
+}
+
+.w-node-_321f052f-cc3c-f722-ea81-06c55e1dda43-9531edc0 {
+grid-column: 3/4;
+}
+
+.app-item-1, .app-item-2 {
+grid-column: 3/4;
+}
+
+.app-title {
+grid-column: 3/4;
+}
+.big-title-wrap {
+grid-column: 1/7;
+}
+.boardgame-title {
+grid-column: 2/3;
+}
+}
+@media screen and (max-width: 767px) {
+.boardgame-item-1, .boardgame-item-2, .boardgame-item-3, .boardgame-item-4, .boardgame-item-5 {
+grid-column-end: 5;
+}
+.big-title-wrap {
+grid-area: span 1 / span 4 / span 1 / span 4;
+}
+.app-item-1, .app-item-2 {
+grid-area: 5/2/6/5;
+}
+.boardgame-title{
+grid-column-end: 5;
+}
+.app-title {
+grid-area: 4/2/5/5;
+}
+}
+</style>
