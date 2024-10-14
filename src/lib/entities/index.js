64e13ee0a23203962b44444cf4a6191f4666fe43
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:12:35 +0300

    upd api

--- src/lib/entities/index.js
+++ src/lib/entities/index.js
@@ -1,4 +1,25 @@
-// import FactSheet from './ui/landing/FactSheet.svelte';
+import VioletOrgCard from './ui/VioletOrgCard.svelte';
+import BlueOrgCard from './ui/BlueOrgCard.svelte';
+import GreenOrgCard from './ui/GreenOrgCard.svelte';
+import YellowOrgCard from './ui/YellowOrgCard.svelte';
+import PurpleOrgCard from './ui/PurpleOrgCard.svelte';
+// import VioletOrgCard from './ui/VioletOrgCard.svelte';
+
+// landing
+import FeatureCard from './ui/landing/FeatureCard.svelte';
+// import VioletOrgCard from './ui/landing/VioletOrgCard.svelte';
+// import VioletOrgCard from './ui/landing/VioletOrgCard.svelte';
+// import VioletOrgCard from './ui/landing/VioletOrgCard.svelte';
 
 
-// export { FactSheet };
+// app
+import RolesList from './ui/app/RolesList.svelte';
+// import VioletOrgCard from './ui/app/VioletOrgCard.svelte';
+// import VioletOrgCard from './ui/app/VioletOrgCard.svelte';
+// import VioletOrgCard from './ui/app/VioletOrgCard.svelte';
+// import VioletOrgCard from './ui/app/VioletOrgCard.svelte';
+
+
+export {
+  VioletOrgCard, BlueOrgCard, GreenOrgCard, YellowOrgCard, PurpleOrgCard, FeatureCard, RolesList,
+};
--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -1,4 +1,61 @@
-// import antISystems from './data/antISystems';
+import Citate from './ui/Citate.svelte';
+import P from './ui/P.svelte';
+// import Citate from './ui/Citate.svelte';
+// import Citate from './ui/Citate.svelte';
+// import Citate from './ui/Citate.svelte';
+// import Citate from './ui/Citate.svelte';
+// import Citate from './ui/Citate.svelte';
 
 
-// export { };
+// utils
+// import socketIoHandler from './utils/socketIoHandler.js';
+// import webSocketPluginVite from './utils/webSocketPluginVite.js';
+
+
+// stores
+import {
+  rollDice, lives, progress, correctAnswers, incorrectAnswers, answerQuestion,
+} from './stores/levelsScore.js';
+import {
+  resetTimer, timer, startTimer, isTimerRunning,
+} from './stores/timer'
+import {
+ rolesList, randomRole, userRole, getRoleMessage,
+} from './stores/roles'
+import {
+  beigeProgress,
+  // blueProgress,
+  // greenProgress,
+  // orangeProgress,
+  // pinkProgress,
+  // purpleProgress,
+  // redProgress,
+  // turquoiseProgress,
+  // yellowProgress,
+
+} from './stores/organisations'
+
+
+// data
+import categories from './data/categories';
+import {beigeQuotes} from './data/citates';
+import footerLinks from './data/footerLinks';
+import faq from './data/faq';
+
+export {
+    beigeProgress,
+  // blueProgress,
+  // greenProgress,
+  // orangeProgress,
+  // pinkProgress,
+  // purpleProgress,
+  // redProgress,
+  // turquoiseProgress,
+  // yellowProgress,
+  beigeQuotes,
+  rolesList,
+  faq, 
+
+  Citate, P, rollDice, footerLinks, categories, randomRole, timer, userRole, getRoleMessage, answerQuestion, resetTimer, lives, progress, correctAnswers, incorrectAnswers, startTimer, isTimerRunning,
+}
+
