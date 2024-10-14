421d464c681ddbb36ce8049b879acc7eb3c30b68
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:13:12 +0300

    refactoring data import

---
+++ src/lib/shared/data/faq.js
@@ -0,0 +1,91 @@
+const faq = [
+  {
+    // Какие цели у проекта?
+    q: 'Jakie są cele projektu?',
+    a: [
+      // Основная цель проекта — рассказать об основах теории спиральной динамики с помощью геймификации. Проект помогает понять, как разные уровни сознания и организации функционируют на практике.
+      `Głównym celem projektu jest przedstawienie podstaw teorii dynamiki spiralnej poprzez grywalizację. Projekt pomaga zrozumieć, jak różne poziomy świadomości i organizacji funkcjonują w praktyce.`,
+      // ``,
+    ]
+  },
+  {
+    q: 'Jaką rolę pełni aplikacja webowa w grze?',
+    a: [
+      // Веб-приложение предоставляет возможность дистанционного игрового процесса, а также дополняет настольную игру, предоставляя дополнительные материалы, которые помогают глубже погрузиться в темы спиральной динамики.
+      `Aplikacja webowa zapewnia zdalną rozgrywkę i uzupełnia grę planszową o dodatkowe materiały, które pomagają zagłębić się w tematykę dynamiki spirali.`,
+      // ``,
+    ]
+  },
+  {
+    // На кого ориентирован этот проект?
+    q: 'Do kogo skierowany jest ten projekt?',
+    a: [
+      // Для широкого круга людей: от бизнес-специалистов и HR, до тренеров, преподавателей и студентов, интересующихся личностным ростом, управлением и развитием организаций.
+      `Dla szerokiego grona osób, od przedsiębiorców i specjalistów HR, po trenerów, nauczycieli i studentów zainteresowanych rozwojem osobistym, zarządzaniem i rozwojem organizacji.`,
+      // ``,
+    ]
+  },
+  {
+    // Как долго длится игровая сессия?
+    q: 'Jak długo trwa sesja rozgrywki?',
+    a: [
+      // Обычно одна игровая сессия длится от 90 до 160 минут, в зависимости от количества и опыта участников.
+      `Zazwyczaj jedna sesja gry trwa od 90 do 160 minut, w zależności od liczby i doświadczenia uczestników.`,
+    ]
+  },
+  {
+    // Можно ли играть в одиночку?
+    q: 'Czy można grać w pojedynkę?',
+    a: [
+      // Игра изначально разработана для групповых сессий, в индивидуальном режиме возможно изучение материалов - но автор проекта не реккомендует данный способ.
+      `Gra została pierwotnie zaprojektowana do sesji grupowych, w trybie indywidualnym możliwe jest studiowanie materiałów - jednak autor projektu nie zaleca tej metody.`,
+      // ``,
+    ]
+  },
+  {
+    // Нужны ли специальные знания для участия в игре?
+    q: 'Czy do udziału w grze potrzebna jest jakaś specjalna wiedza?',
+    a: [
+      // Нет, игра разработана так, чтобы её могли понять даже новички. Все основные концепции и правила объясняются по ходу игры. Предварительное знакомство со спиральной динамикой или методиками менеджмента будет полезным, но не обязательным.
+      `Nie, gra została zaprojektowana tak, aby była zrozumiała nawet dla początkujących. Wszystkie podstawowe pojęcia i zasady są wyjaśniane w miarę postępów w grze. Wcześniejsza znajomość dynamiki spiralnej lub technik zarządzania jest pomocna, ale nie jest wymagana.`,
+      // ``,
+    ]
+  },
+  {
+    // Что нужно для начала игры?
+    q: 'Co jest potrzebne do rozpoczęcia gry?',
+    a: [
+      // Для начала игры вам понадобятся базовый набор настольной игры и устройство с доступом к веб-приложению.
+      `Aby rozpocząć grę, potrzebny będzie podstawowy zestaw do gry planszowej i urządzenie z dostępem do aplikacji webowej.`,
+      // ``,
+    ]
+  },
+  {
+    // Можно ли использовать игру в образовательных и тренинговых целях?
+    q: 'Czy gra może być wykorzystywana do celów edukacyjnych i szkoleniowych?',
+    a: [
+      // Да, игра прекрасно подходит для использования на тренингах, корпоративных обучениях и в образовательных учреждениях. Она помогает развивать навыки системного мышления, управления и работы с различными организационными моделями.
+      `Tak, gra jest idealna do wykorzystania w szkoleniach, treningach korporacyjnych i instytucjach edukacyjnych. Pomaga rozwijać umiejętności myślenia systemowego, zarządzania i pracy z różnymi modelami organizacyjnymi.`,
+      ``,
+    ]
+  },
+  {
+    // Проект уже закончен?
+    q: 'Czy projekt jest już ukończony?',
+    a: [
+      // Проект продолжает развиваться. В приложение добавляются сведения о новых организациях, в игровой процесс добавляются предложения от сообщества.
+      `Projekt wciąż ewoluuje. Informacje o nowych organizacjach są dodawane do aplikacji + propozycje od społeczności są dodawane do rozgrywki.`,
+      // ``,
+    ]
+  },
+  // {
+  //   q: '',
+  //   a: [
+  //     ``,
+  //     ``,
+  //   ]
+  // },
+
+]
+
+export default faq;
---
+++ src/lib/widgets/landing/Faq.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { faq } from '$lib/shared'
+</script>
+
+<div class="section">
+  <div class="wrapper">
+    <div class="w-layout-grid _12-col">
+      <div  class="headline">
+        <h2 class="header-absolute">FAQ</h2>
+        <div  class="section-gradient">
+          <img src="/images/rounded_section_gradient.png" loading="lazy" alt="" class="saturate">
+        </div>
+      </div>
+    </div>
+    <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12 lg:pl-40 md:pl-20">
+        {#each faq as {q, a}}
+          <div class="">
+            <div >
+              <p>
+                <strong>{q}</strong>
+              </p>
+              {#each a as answer}
+                <p>
+                  {answer}
+                </p>
+              {/each}
+          </div>
+          </div>
+        {/each}
+      </div>
+    <div class="line-section"></div>
+  </div>
+</div>
+
+
+<style>
+
+</style>
