7f06370d5c847f8a2115981492d4be16d7e1aade
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:12:00 +0300

    stable v.

---
+++ src/lib/widgets/landing/Features.svelte
@@ -0,0 +1,57 @@
+<script>
+
+</script>
+
+<section class="section">
+  <div class="wrapper">
+    <div class="_12-col above">
+      <div id="w-node-e08ef7de-ca54-8f2f-cc48-69f6685e8a34-9531edc0"  class="headline-text">
+        <h3>Interaktywna edukacja</h3>
+        <p>Gra planszowa połączona z aplikacją webową w celu zwiększenia możliwości uczenia się i dogłębnej analizy sytuacji. Nadaje się do szkoleń korporacyjnych, coachingu i imprez integracyjnych, w których ważne jest rozwijanie umiejętności zarządzania i przywództwa.</p>
+      </div>
+      <div id="w-node-_3db2bbcd-1602-d7e0-a305-f940535506d8-9531edc0" class="number-container">
+        <img src="/images/number_1.svg" loading="eager" alt="" class="number z-20">
+        <img src="/images/circle_teal.png" loading="eager" alt="" class="number-background h-34 w-34 saturate">
+      </div>
+    </div>
+    <div class="_12-col">
+      <div id="w-node-_5db2c8cc-e831-1bb0-0eac-f713b532132c-9531edc0" class="number-container">
+        <img src="/images/number_2.svg" loading="eager" alt="" class="number z-20">
+        <img src="/images/circle_yellow.png" loading="eager" alt="" class="number-background h-34 w-34 saturate">
+      </div>
+      <div id="w-node-_5db2c8cc-e831-1bb0-0eac-f713b5321327-9531edc0" class="headline-text">
+        <h3>Rodzaje struktur organizacyjnych</h3>
+        <p>Rozwijanie umiejętności myślenia systemowego i wiedzy o różnych typach organizacji i ich charakterystykach - na podstawie rzeczywistych przykładów organizacji.</p>
+      </div>
+    </div>
+    <div class="_12-col">
+      <div id="w-node-f58ea8ff-b559-ef1e-813f-f19c27a5fe6a-9531edc0" class="number-container">
+        <img src="/images/number_3.svg" loading="eager" alt="" class="number z-20">
+        <img src="/images/circle_teal.png" loading="eager" alt="" class="number-background h-34 w-34 saturate">
+      </div>
+      <div id="w-node-f58ea8ff-b559-ef1e-813f-f19c27a5fe65-9531edc0" class="headline-text">
+        <h3>Lista praktyk</h3>
+        <p>Lista praktyk,  metod oraz mechanizmów zarządzania dla różnych stylów przywództwa i struktur organizacyjnych.</p>
+      </div>
+    </div>
+    <div class="_12-col">
+      <!-- <div id="w-node-e3579d8a-327c-7894-dbea-e7798e48d7b3-9531edc0" class="number-container">
+        <img src="/images/number_4.svg" loading="eager" alt="" class="number z-20">
+        <img src="/images/circle_yellow.png" loading="eager" alt="" class="number-background  h-34 w-34 saturate">
+      </div> -->
+      <div id="w-node-e3579d8a-327c-7894-dbea-e7798e48d7ae-9531edc0" class="headline-text no-margin-bottom">
+        <h2>50+ organizacji</h2>
+        <h2>100+ metod i praktyk</h2>
+
+
+        <!-- <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> -->
+      </div>
+    </div>
+    <div class="line-section"></div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
