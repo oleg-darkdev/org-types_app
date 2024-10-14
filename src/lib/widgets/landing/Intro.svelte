bc8d885ec6d49d3f2ca6f3ef0d1c84a80d3cd199
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:19:04 +0300

    alfa v.

---
+++ src/lib/widgets/landing/Intro.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<div  data-w-id="df3251a2-f04a-f329-835f-51cb0ebb01ff" class="storytime">
+  <div class="padding">
+    <div class="storypadding">
+      <div class="columns w-row">
+        <div class="column-2 w-col w-col-6">
+          <div class="twocolbody intro w-clearfix"><span class="dropcap">Lorem Ipsum has been</span>
+  the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+            <strong class="bold-text">Lorem Ipsum has been</strong>
+  the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+            ‍</div>
+        </div>
+        <div class="column w-col w-col-6">
+          <div class="twocolbody intro w-clearfix">
+            <span class="dropcap">Lorem Ipsum has been</span>
+  the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+            <strong class="bold-text">Lorem Ipsum has been</strong>
+  the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  
+</style>
