80e797c144102173b59974587442529f3a793386
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:16:47 +0300

    alfa v.

---
+++ src/lib/widgets/landing/Divider.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<div class="artifactsintro">
+
+  <div id="artifacts" class="pagescrolltag"></div>
+  <div class="signalsheader">
+    <div class="padding w-clearfix">
+      <div class="allcaps">Lorem Ipsum has</div>
+    </div>
+  </div>
+
+  <div class="padding">
+    <div class="div-block-4">
+      <div class="_50">
+        <p>Lorem Ipsum has been the industry's <em>standard dummy </em> text ever since the 1500s <em> when an unknown printer took. </em>
+
+           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+      <div class="_50 _2">
+        <div class="iconbar">
+          <a href="/" class="badge forming-kin w-inline-block"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<style>
+
+</style>
