538521ccc73ab1c2cbc9c14f388f73428d2fb0ba
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:14:04 +0300

    stable v.

---
+++ src/lib/shared/ui/Citate.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let citate;
+</script>
+
+<div class="dustpadding ">
+  <blockquote class="quote">
+    <span class="text-span">“</span>
+    {citate.text}”
+    <br>
+    <span class="byline">{citate.subTitle}</span>
+  </blockquote>
+</div>
+
+<style>
+
+</style>
---
+++ src/lib/shared/ui/P.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let text;
+</script>
+
+<!-- twocolbody -->
+<p class="max-w-lg w-clearfix">
+  <slot></slot>
+  {text}
+</p>
+
+
+<style>
+
+</style>
