201423722ac6e3d583ba26266c19eabf8a3c9775
DarkDev <darkdev_brand@riseup.net>
2024-10-14 22:07:12 +0300

    refactoring card for component

--- src/lib/widgets/landing/OtherBoardgames.svelte
+++ src/lib/widgets/landing/OtherBoardgames.svelte
@@ -1,4 +1,6 @@
 <script>
+  import {OtherBoardgameCard} from '$lib/entities';
+  
   export let otherBoardgames;
 </script>
 
@@ -15,48 +17,9 @@
     </div>
     <div class="projects-outer-wrap">
       <div class="w-dyn-list">
-        <div role="list" class="grid grid-cols-3 gap-4">
+        <div role="list" class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
           {#each otherBoardgames as boardgame}
-            <div role="listitem" class="w-dyn-item max-w-md">
-              <div class="work-piece">
-                <div class="thumbnail-container ">
-                  <img src="/images/logo.svg" loading="lazy" alt="" class="thumbnail-image">
-                </div>
-
-                <div>
-                  <div class="tag work-piece-tag">Lorem Ipsum is simply</div>
-                  <div class="tag work-piece-tag separater">|</div>
-                  <div class="tag work-piece-tag w-dyn-bind-empty"></div>
-                  <h3 class="thumbnail-title">Lorem Ipsum is simply</h3>
-                <div  class="headline-text">
-                  <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                </div>
-
-                  <div class="signallink">
-                  <div class=" ">
-                    <img src="/images/arrow.svg" alt="" class=" arrow">
-
-                    <div class="signalfooter">
-                      <p class="signaltitle">Czytaj więcej</p>
-                    </div>
-                  </div>
-                </div>
-                </div>
-                <div class="get-yours w-condition-invisible">
-                  <h3 class="thumbnail-title centered">Lorem Ipsum is simply</h3>
-                  <div class="button-container get-yours">
-                    <a href="/" class="button w-button">Lorem Ipsum</a>
-                    <a href="/" class="secondary-link">Lorem Ipsum is simply</a>
-                  </div>
-
-
-                </div>
-              <!-- <div class="signal"> -->
-
-                </div>
-
-              <!-- </div> -->
-            </div>
+            <OtherBoardgameCard {boardgame} />
           {/each}
         </div>
       </div>
