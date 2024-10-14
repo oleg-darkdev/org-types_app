9af325b5dbba0c2004ae5018b421317889366a31
DarkDev <darkdev_brand@riseup.net>
2024-10-14 21:13:30 +0300

    beta v.

---
+++ src/lib/widgets/landing/CtaColoured.svelte
@@ -0,0 +1,49 @@
+<script>
+
+</script>
+
+<div class="section">
+  <div class="wrapper">
+    <div class="w-layout-grid _12-col cta-full-panel">
+      <div class="text-wrap cta-content">
+        <h1 class="above">
+          Lorem Ipsum is
+          <em class="special-word">simply</em>.
+           dummy text of the printing and typesetting industry.
+         </h1>
+        <div class="button-container margin-top-80 above">
+          <a href="/" class="button w-button">Get Started</a>
+        </div>
+      </div>
+      <div class="img-wrap cta-gradient-container">
+        <img src="/images/cta_coloured_gradient.png" loading="lazy" alt="" class="cta-gradient-first">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<style>
+.img-wrap {
+  grid-area: 1/1/2/13;
+}
+.text-wrap{
+  grid-area: 1/2/2/12;
+}
+@media screen and (max-width: 991px) {
+  .img-wrap {
+    grid-column-end: 7;
+  }
+    .text-wrap{
+    grid-column: 1/7;
+  }
+}
+@media screen and (max-width: 767px) {
+  .img-wrap {
+    grid-column-end: 5;
+  }
+    .text-wrap{
+    grid-column-end: 5;
+  }
+}
+</style>
---
+++ src/lib/widgets/landing/CtaLight.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<div class="section">
+  <div class="wrapper">
+    <div class="w-layout-grid _12-col cta-full-panel _2nd">
+      <div id="w-node-_7920e670-43c9-f249-1431-49e20b88b823-9531edc0" class="cta-content">
+        <h1 class="above">Hit us up for <em class="special-word">quotes</em> and we'll get back to you shortly. </h1>
+        <div class="button-container margin-top-80 above">
+          <a href="/" class="button w-button">
+            Więcej o dynamiki spiralnej
+          </a>
+        </div>
+      </div>
+      <div id="w-node-_7920e670-43c9-f249-1431-49e20b88b82c-9531edc0" class="cta-gradient-container">
+        <img src="images/gradient_cta_light.png" loading="lazy"  alt="" class="cta-gradient-first">
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+
+</style>
